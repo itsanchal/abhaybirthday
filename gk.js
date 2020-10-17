f1522b8def73139b687af04ad6aaceae8c3e07a7
itsanchal <66128186+itsanchal@users.noreply.github.com>
2020-10-17 06:47:15 +0530

    Add files via upload

---
+++ gk.js
@@ -0,0 +1,8 @@
+alert("click on cake to enjoy ur birthday party online")
+window.addEventListener("mouseover",play);
+window.addEventListener('click', play);
+function play()
+ {
+
+	document.getElementById("a").play();
+}
